7b309b0602b8b141384a3bac795cc089db7bc82e
Carlos Quinones <CQuinones@QuEnterprises.com>
2023-02-28 00:04:50 -0500

    Add project files.

---
+++ QuEnterprises.com/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace QuEnterprises.com.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
